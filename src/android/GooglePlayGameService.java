b919e9a3693b6a90472d1e63b48a1da1a1197184
lycwed <ylechevalier@gmail.com>
2017-07-27 12:05:55 +0200

    add new files

---
+++ src/android/GooglePlayGameService.java
@@ -0,0 +1,20 @@
+package com.lycwed;
+
+public class GoogleGameService extends BaseGameActivity{
+
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doSignIn() {
+		beginUserInitiatedSignIn();
+	}
+}
